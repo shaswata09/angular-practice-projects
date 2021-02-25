521e9d163b495da0f751d5dd0ef3e22b3990176e
Shaswata Mitra <shaswatamitra@gmail.com>
2021-02-25 22:14:49 +0530

    Video:2-22 >> Working with Component Styles

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
