7217510a561297882c23d0e9518ce46ec69e1eeb
Shaswata Mitra <shaswatamitra@gmail.com>
2021-02-25 22:30:21 +0530

    Video:2-23 >> Fully Understanding the component Selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers',  // selecting by Component [Standard Option]
+  // selector: '[app-servers]',   // selecting by Attribute [Other Options]
+  // selector: `.app-servers`,    // selecting by Class  [Other Options]
+  // NOTE: Selecting by ID is not supported in Angular
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
