0ec802c4f4ef48ce8dcb94dacbc8f0ff4240279d
Shaswata Mitra <shaswatamitra@gmail.com>
2021-02-27 02:02:51 +0530

    Assignment 1 code changes

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import {SuccessfulMessageComponent} from './successful-message/successful-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningMessageComponent,
+    SuccessfulMessageComponent
   ],
   imports: [
       BrowserModule,
---
+++ my-first-app/src/app/successful-message/successful-message.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-successful-message',
+  templateUrl: './successful-message.component.html',
+  styleUrls: ['./successful-message.component.css']
+})
+
+export class SuccessfulMessageComponent{ }
