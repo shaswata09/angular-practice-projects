66f87a7da4305d910e8f4d7476f7155f11e8bce7
shaswata <shaswatamitra@gmail.com>
2022-01-11 21:18:25 -0600

    Video: 5-76 >> Getting access to the Template & DOM with @ViewChild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
   selector: "app-cockpit",
@@ -15,7 +22,8 @@ export class CockpitComponent implements OnInit {
     serverContent: string;
   }>();
   // newServerName = "";
-  newServerContent = "";
+  // newServerContent = "";
+  @ViewChild("serverContentInput") serverContentInput: ElementRef;
 
   constructor() {}
 
@@ -24,14 +32,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(serverInputElement: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverInputElement.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(serverInputElement: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: serverInputElement.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
