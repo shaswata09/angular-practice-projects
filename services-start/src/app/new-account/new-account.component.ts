c9e7799d4c49d820c8012cb8dc0140d91b5df307
shaswata <shaswatamitra@gmail.com>
2022-01-13 00:09:46 -0600

    Video: 9-107 >> Creating a Logging Service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { LoggingService } from "../logging.service";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status: accountStatus
+      status: accountStatus,
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // This is not proper way to use services in Angular
+    const service = new LoggingService();
+    service.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
